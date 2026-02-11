b8c2ca8d987b5ac498da5e86adada3449b0c12fe
Hollebeque <hollebeque.fabien@hotmail.com>
2026-02-11 16:50:43 +0100

    Ajout du formulaire de signature

--- src/Controller/Admin/Security/UserController.php
+++ src/Controller/Admin/Security/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller\Admin\Security;
 
+use App\ControllerHandler\Admin\Security\UserControllerHandler;
 use App\DTO\Admin\Security\UserEditDTO;
 use App\Entity\User\User;
 use App\Form\Admin\Security\UserEditType;
+use App\Form\Security\SignatureType;
 use App\Repository\User\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,7 +18,8 @@ use Symfony\Component\Routing\Attribute\Route;
 final class UserController extends AbstractController
 {
     public function __construct(
-        private readonly UserRepository $userRepository
+        private readonly UserRepository $userRepository,
+        private readonly UserControllerHandler $userControllerHandler
     ) {}
 
     #[Route('/', name: 'index')]
@@ -42,10 +45,16 @@ final class UserController extends AbstractController
         $userDto = UserEditDTO::fromUser($user);
 
         $userForm = $this->createForm(UserEditType::class, $userDto)->handleRequest($request);
+        $signatureForm = $this->createForm(SignatureType::class, $user)->handleRequest($request);
+
+        if ($this->userControllerHandler->editSignature($signatureForm, $user)) {
+            return $this->redirectToRoute('admin_user_index');
+        }
 
         return $this->render('admin/security/edit.html.twig', [
             'user' => $user,
-            'userForm' => $userForm->createView()
+            'userForm' => $userForm->createView(),
+            'signatureForm' => $signatureForm->createView()
         ]);
     }
 }
--- src/ControllerHandler/Admin/Security/UserControllerHandler.php
+++ src/ControllerHandler/Admin/Security/UserControllerHandler.php
@@ -2,18 +2,24 @@
 
 namespace App\ControllerHandler\Admin\Security;
 
+use App\Entity\User\User;
 use App\Repository\User\UserRepository;
+use Symfony\Component\Form\FormInterface;
 
-class UserControllerHandler
+readonly class UserControllerHandler
 {
     public function __construct(
         private UserRepository $userRepository
     ) {}
 
-    public function edit(): bool
+    public function editSignature(FormInterface $form, User $user): bool
     {
-        $this->userRepository->update();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->userRepository->update($user);
 
-        return true;
+            return true;
+        }
+
+        return false;
     }
 }
